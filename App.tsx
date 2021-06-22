314b0fe3d7fd6de46607ecfc19a5cc2f621093f7
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-06-22 01:20:28 -0300

    Custom text fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
 import { SignIn } from './src/screens/SingIn';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded) {
+    <AppLoading />
+  }
+
   return (
-    <SignIn></SignIn>
+    <>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn /> 
+    </>
   );
 };
--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -3,7 +3,6 @@ import {
     Text,
     View, 
     Image,
-    StatusBar
 } from 'react-native';
 import { styles } from './styles';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -13,11 +12,6 @@ import IllustrationImg from '../../assets/illustration.png';
 export const SignIn = () => {
     return (
         <View style={styles.container} >
-            <StatusBar 
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            /> 
             <Image 
                 source={IllustrationImg} 
                 style={styles.image}
@@ -25,9 +19,9 @@ export const SignIn = () => {
 
             <View style={styles.content} >
                 <Text style={styles.title} >
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e oraganize suas {'\n'}
+                    jogatinas
                 </Text>
                 <Text style={styles.subtitle} >
                     Crie grupos para jogar seus games {`\n`}
