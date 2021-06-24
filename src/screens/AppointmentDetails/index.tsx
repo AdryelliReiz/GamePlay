59cd30b3ff0c2ca1bbc3efe73ed6f79c518c187f
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-06-24 00:49:33 -0300

    AppointmentDetails screen

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View,
+    Text,
+    ImageBackground, 
+    FlatList,
+} from 'react-native';
 import  { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
+import BannerImg from '../../assets/banner.png';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
-
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
+import { ListDivider } from '../../components/ListDivider';
+import { ButtonIcon } from '../../components/ButtonIcon';
 
 export const AppointmentDetails = () => {
+    const members = [
+        {
+            id: '1',
+            username: '!ADRY',
+            avatar_url: 'https://github/AdryelliReiz.png' ,
+            status: 'online',
+        },
+        {
+            id: '2',
+            username: 'Rodrigo',
+            avatar_url: 'https://github/rodrigorgtic.png' ,
+            status: 'offline',
+        }
+    ]
+
     return (
         <Background>
             <Header
@@ -25,6 +49,43 @@ export const AppointmentDetails = () => {
                     </BorderlessButton>
                 }
             />
+
+            <ImageBackground 
+                source={BannerImg} 
+                style={styles.banner}
+            >
+               <View style={styles.bannerContent} >
+                    <Text style={styles.title} >
+                        Lendários
+                    </Text>
+
+                    <Text style={styles.subtitle} >
+                        É hoje que vamos para o torneio mundial de minecraf
+                    </Text>
+               </View>
+
+            </ImageBackground>
+
+            <ListHeader 
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+            <FlatList 
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Member  data={item}/>
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+
+            <View style={styles.footer} >
+                <ButtonIcon 
+                    title="Entrar na partida"
+                />
+            </View>
+            
         </Background>
     )
 };
